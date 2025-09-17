e31e5070113d6ef227e32ca806ea738001de5e7f
siyanet <siyangetahunw@gmail.com>
2025-09-17 04:16:12 +0300

    fixing conflict

--- tenants/management/commands/create_db_management.py
+++
@@ -1,9 +0,0 @@
-# from django.db import connections
-# from django.db.utils import ProgrammingError
-
-# def create_tenant_database(db_name):
-#     with connections['default'].cursor() as cursor:
-#         try:
-#             cursor.execute(f"CREATE DATABASE {db_name}")
-#         except ProgrammingError:
-#             pass  # Database already exists
--- urls_debug.py
+++
@@ -1,4 +0,0 @@
-from django.urls import get_resolver
-
-for url_pattern in get_resolver().url_patterns:
-    print(url_pattern)
