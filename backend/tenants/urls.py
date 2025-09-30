cdb5f20be480ed5c7f111afd4ceedef997633983
siyanet <siyangetahunw@gmail.com>
2025-09-30 10:30:54 +0300

    Remove __pycache__, db, venv, env, media from git tracking

--- backend/tenants/urls.py
+++
@@ -1,8 +0,0 @@
-# # core/urls.py
-from django.urls import path
-from .views import TenantRegistrationView,TenantInfoView
-
-urlpatterns = [
-    path('register/', TenantRegistrationView.as_view(), name='register-tenant'),
-     path('tenant-info/', TenantInfoView.as_view(), name='tenant-info'),
-]
