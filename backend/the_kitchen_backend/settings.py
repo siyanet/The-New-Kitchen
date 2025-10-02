6d4d656b90ad9f1b2b3565b2711d2ab37dc37bbd
siyanet <siyangetahunw@gmail.com>
2025-10-02 17:40:26 +0300

    fixing the setting

--- backend/the_kitchen_backend/settings.py
+++ backend/the_kitchen_backend/settings.py
@@ -251,8 +251,8 @@ STORAGES = {
     },
 }
 
-# STATICFILES_STORAGE = "cloudinary_storage.storage.StaticCloudinaryStorage"
-# DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+STATICFILES_STORAGE = "cloudinary_storage.storage.StaticCloudinaryStorage"
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 CLOUDINARY_STORAGE = {
     "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
     "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
