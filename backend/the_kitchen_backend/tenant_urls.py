30c1bd80ae0c16a36de34efd44e7cbde202ef79b
siyanet <siyangetahunw@gmail.com>
2025-10-03 01:38:33 +0300

    fix url

---
+++ backend/the_kitchen_backend/tenant_urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path("api/auth/", include("djoser.urls")),
+    path("api/auth/", include("djoser.urls.jwt")),
+    path("api/users/", include("users.urls")),
+    path("api/restaurant/", include("restaurant.urls")),
+    path("api/items/", include("items.urls")),
+    path("api/staffs/", include("staff.urls")),
+    path("api/orders/", include("order.urls")),
+]
