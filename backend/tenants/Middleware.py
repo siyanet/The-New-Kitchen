e642768197363001e93fb691c83965416fadfd8b
siyanet <siyangetahunw@gmail.com>
2025-10-02 19:42:51 +0300

    redploy2

--- backend/tenants/Middleware.py
+++ backend/tenants/Middleware.py
@@ -15,6 +15,32 @@ from .tenant_context import set_current_tenant
 #         response = self.get_response(request)
 #         return response
 
+# class TenantMiddleware:
+#     def __init__(self, get_response):
+#         self.get_response = get_response
+
+#     def __call__(self, request):
+#         hostname = request.get_host().split(':')[0]
+#         tenant = None
+
+#         if 'localhost' in hostname or 'render.com' in hostname:
+#             # get tenant from path: /t/<tenant>/
+#             path_parts = request.path.strip('/').split('/')
+#             if len(path_parts) > 1 and path_parts[0] == 't':
+#                 tenant_name = path_parts[1]
+#                 tenant = Tenant.objects.filter(name=tenant_name).first()
+#         else:
+#             tenant = Tenant.objects.filter(domain=hostname).first()
+
+#         if not tenant:
+#             raise Http404("Tenant not found")
+
+#         set_current_tenant(tenant)
+#         response = self.get_response(request)
+#         return response
+
+
+
 class TenantMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -23,12 +49,11 @@ class TenantMiddleware:
         hostname = request.get_host().split(':')[0]
         tenant = None
 
-        if 'localhost' in hostname or 'render.com' in hostname:
-            # get tenant from path: /t/<tenant>/
-            path_parts = request.path.strip('/').split('/')
-            if len(path_parts) > 1 and path_parts[0] == 't':
-                tenant_name = path_parts[1]
-                tenant = Tenant.objects.filter(name=tenant_name).first()
+        # Always try to get tenant from path first
+        path_parts = request.path.strip('/').split('/')
+        if len(path_parts) > 1 and path_parts[0] == 't':
+            tenant_name = path_parts[1]
+            tenant = Tenant.objects.filter(name=tenant_name).first()
         else:
             tenant = Tenant.objects.filter(domain=hostname).first()
 
@@ -36,6 +61,4 @@ class TenantMiddleware:
             raise Http404("Tenant not found")
 
         set_current_tenant(tenant)
-        response = self.get_response(request)
-        return response
-
+        return self.get_response(request)
