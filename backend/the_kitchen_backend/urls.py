5551462d930281f0500d5579de01ee0669d09033
siyanet <siyangetahunw@gmail.com>
2025-10-03 01:34:09 +0300

    fixing url

--- backend/the_kitchen_backend/urls.py
+++ backend/the_kitchen_backend/urls.py
@@ -21,25 +21,14 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('api/tenant/', include('tenants.urls')),
-     path('t/<str:ignored>/', include([
-        path('api/auth/', include('djoser.urls')),
-        path('api/auth/', include('djoser.urls.jwt')), 
-        path('api/users/', include('users.urls')),
-        path('api/restaurant/', include('restaurant.urls')),
-        path('api/items/', include('items.urls')),
-        path('api/staffs/', include('staff.urls')),
-        path('api/orders/', include('order.urls')),
-       
-    ])),
-    # path('api/auth/', include('djoser.urls')),
-    # path('api/auth/', include('djoser.urls.jwt')), 
-    # path('api/users/',include('users.urls')),
-    # path('api/restaurant/', include('restaurant.urls')),
-    # path('api/items/', include('items.urls')),
-    # path('api/staffs/',include("staff.urls")),
-    # path('api/orders/',include("order.urls")),
-    # path('api/tenant/', include('tenants.urls')),
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.jwt')), 
+    path('api/users/',include('users.urls')),
+    path('api/restaurant/', include('restaurant.urls')),
+    path('api/items/', include('items.urls')),
+    path('api/staffs/',include("staff.urls")),
+    path('api/orders/',include("order.urls")),
+    path('api/tenant/', include('tenants.urls')),
 
 ]
 
